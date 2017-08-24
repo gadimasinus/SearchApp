e3906f37f28f3b4c179987fa80d5c1a940b71a51
Manish Gadiya <Manish Gadiya>
2017-08-24 16:32:01 -0400

    apply styling to the elememt

--- src/app/imagesearch/imagesearch.component.ts
+++ src/app/imagesearch/imagesearch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-imagesearch',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesearchComponent implements OnInit {
 
+  @Input() searchWord : string = "Default";
   constructor() { }
 
   ngOnInit() {
